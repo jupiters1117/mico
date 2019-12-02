d1ac80808f5bf76eb6a2ab66cdeb8bd7c4d380b1
KuoLing Huang <jupiters1117@gmail.com>
2019-12-01 19:38:14 -0600

    Added doc.

---
+++ _downloads/5752dac0a9250f4dc406153a0cb1cd1d/fuck.py
@@ -0,0 +1,15 @@
+"""
+FUCK
+====
+
+"""
+from mico import MutualInformationForwardSelection
+import pandas as pd
+from sklearn.datasets import load_breast_cancer, load_diabetes
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    print("FUCK")
+
+    plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+    plt.show()
--- check.py
+++
@@ -1,7 +0,0 @@
-import colinpy
-print(dir(colinpy))
-
-import mico
-print(dir(mico))
-import sys
-print("\n".join(sys.path))
